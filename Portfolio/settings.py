908aa043852ad5b40544cead96afbb35664d7cb0
Pfenei <adrsanrod2@gmail.com>
2021-06-11 03:34:05 +0100

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pfenei_portfolio',
-        'USER': 'pfenei_knov',
-        'PASSWORD': 'us25av4c356Ytr67@',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
